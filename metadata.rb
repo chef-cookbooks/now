b50d719919259defb3331e68d4264fd06638ea0d
Sean OMeara <someara@opscode.com>
2014-01-03 13:25:57 -0500

    updating license and author bits

--- metadata.rb
+++ metadata.rb
@@ -1,1 +1,7 @@
-name 'now'
+name              'now'
+maintainer        'Chef'
+maintainer_email  'cookbooks@getchef.com'
+license           'Apache 2.0'
+description       'library cookbook providing special ordering method include_recipe_now'
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version           '0.1.0'
